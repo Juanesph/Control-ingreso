8411f9fcf07c085dd991d6cc3e09b6f7b9adb108
Juanesph <juanesph@gmail.com>
2024-08-28 18:21:10 -0400

    eliminado errores

--- client/src/components/ExportButton.component.jsx
+++ client/src/components/ExportButton.component.jsx
@@ -16,12 +16,12 @@ const ExportButton = ({ data, fileName }) => {
     return csvData;
   };
 
-  const exportToExcel = () => {
+ /*  const exportToExcel = () => {
     const ws = XLSX.utils.json_to_sheet(data);
     const wb = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(wb, ws, "Sheet 1");
     XLSX.writeFile(wb, fileName);
-  };
+  }; */
 
   return (
     <div>
